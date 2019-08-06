6a52a3f456348c6a813d6b56c258e159a2281660
Chris Stead <cmstead@gmail.com>
2019-08-05 21:23:48 -0700

    Added commit opt-out on test special case run

--- app/test-and-commit/testAndCommit.js
+++ app/test-and-commit/testAndCommit.js
@@ -1,16 +1,38 @@
 function testAndCommit(
     changeCommitHelper,
     testRunner,
+    inquirer
 ) {
+
+    function commitChangesOnApproval() {
+        inquirer
+            .prompt([
+                {
+                    name: 'okToCommit',
+                    message: 'Special test case run, do you want to commit?',
+                    type: 'confirm'
+                }
+            ])
+            .then(function(data) {
+                if(data.okToCommit) {
+                    changeCommitHelper.commitChanges();
+                }
+            });
+    }
+
     return function (args) {
 
         try {
             testRunner.runTests(args);
 
-            changeCommitHelper.commitChanges();
+            if(args.length > 0) {
+                commitChangesOnApproval()
+            } else {
+                changeCommitHelper.commitChanges();
+            }
         } catch (e) {
             console.log('Tests failed, skipping commit.');
-            
+
             process.exit(1);
         }
     }
